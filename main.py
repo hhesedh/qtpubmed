ffad2cdfe34c7fe88b888bd6c9ae1aa2bcc7f68b
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-05-17 23:22:51 -0300

    adição do modulo pubmed no arquivo

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import sys
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QMainWindow, QProgressBar, QLineEdit, QLabel, QPushButton
 from PyQt5.QtCore import QSize
+from pymed import PubMed
 
 
 class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
+
+        self.pubmed = PubMed(tool="MyTool", email="my@email.address")
+
         self.setMinimumSize(QSize(320, 140))
         self.setWindowTitle("PyQt Line Edit example (textfield) - pythonprogramminglanguage.com")
 
@@ -19,34 +23,27 @@ class Example(QMainWindow):
         self.line.resize(200, 32)
         self.nameLabel.move(20, 20)
 
-        pybutton = QPushButton('OK', self)
-        pybutton.clicked.connect(self.click_method)
-        pybutton.resize(200, 32)
-        pybutton.move(80, 60)
+        self.pybutton = QPushButton('OK', self)
+        self.pybutton.clicked.connect(self.click_method)
+        self.pybutton.resize(200, 32)
+        self.pybutton.move(80, 60)
 
         self.pbar = QProgressBar(self)
         self.pbar.setGeometry(30, 40, 200, 25)
         self.pbar.move(80, 100)
-        self.pbar.setValue(50)
+        self.pbar.setValue(0)
 
         self.setWindowTitle("QT Progressbar Example")
         self.setGeometry(32, 32, 320, 200)
         self.show()
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.handle_timer)
-        self.timer.start(1000)
-
     def click_method(self):
-        print(self.line.text())
+        results = self.pubmed.getTotalResultsCount(self.line.text())
+        print(results)
+        self.handle_timer(results)
 
-    def handle_timer(self):
-        value = self.pbar.value()
-        if value < 100:
-            value = value + 1
-            self.pbar.setValue(value)
-        else:
-            self.timer.stop()
+    def handle_timer(self, results):
+        self.pbar.setValue(results)
 
 
 if __name__ == '__main__':
